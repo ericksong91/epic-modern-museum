b20f2e9a950d3d758987eb78c6c6718d73e190e6
Eric Song <ericksong91@gmail.com>
2023-04-01 12:42:27 -0400

    Added PW digest to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :paintings
     has_many :museums, through: :paintings
 end
--- db/migrate/20230331205320_create_users.rb
+++ db/migrate/20230331205320_create_users.rb
@@ -1,8 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :username, :password_digest
-      t.string :bio
+      t.string :username, :password_digest, :bio
 
       t.timestamps
     end
