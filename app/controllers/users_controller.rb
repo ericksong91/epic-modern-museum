576959044989edc69d53afd038560d51c22ffa96
Eric Song <ericksong91@gmail.com>
2023-04-01 16:40:58 -0400

    Added user creation, login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id 
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: { errors: "No user found" }, status: :unauthorized
+        end
+    end
+
+    private 
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :bio)
+    end
 end
