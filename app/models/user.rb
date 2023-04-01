7d1f8ae07400577f808e94502e2ce49222cccc46
Eric Song <ericksong91@gmail.com>
2023-04-01 16:02:27 -0400

    Added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
     validates :username, presence: true
     validates :username, uniqueness: true
+    validates :password_digest, presence: true
 
     has_secure_password
+    
     has_many :paintings
     has_many :museums, through: :paintings
 end
