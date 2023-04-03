01ebe570f9fa37d672ec9ed77134d003e1a40af9
Eric Song <ericksong91@gmail.com>
2023-04-03 16:23:38 -0400

    Try another deploy again

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,7 @@ cp -a client/build/. public/
 
 # builds the back end code
 bundle install
+bundle exec rails db:drop
+bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed # if you have seed data, run this command for the initial deploy only
