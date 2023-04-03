5d37c2efafb9b3056424ed19db56f748238f933f
Eric Song <ericksong91@gmail.com>
2023-04-03 14:54:43 -0400

    Try another deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,20 +11,20 @@ require 'faker'
 pp "Now Seeding Users..."
 
 5.times do
-    user = User.create(username: "#{Faker::Name.first_name}##{rand(1000...9999)}", password:"asdf", 
+    user = User.create!(username: "#{Faker::Name.first_name}##{rand(1000...9999)}", password:"asdf", 
         password_confirmation: "asdf", bio: "#{Faker::Lorem.paragraphs(number:1)}")
 end
 
 pp "Now Seeding Museums..."
 
 3.times do
-    museum = Museum.create(name: "Museum of #{Faker::Name.first_name}", location: "#{Faker::Address.city}", bio: "#{Faker::Lorem.paragraphs(number:1)}")
+    museum = Museum.create!(name: "Museum of #{Faker::Name.first_name}", location: "#{Faker::Address.city}", bio: "#{Faker::Lorem.paragraphs(number:1)}")
 end
 
 pp "Now Seeding Paintings..."
 
 10.times do
-    painting = Painting.create(name: "Painting of #{Faker::Name.first_name}", bio: "Super cool picture!", img_url: "#{Faker::LoremFlickr.image}", year: rand(2006..2023),
+    painting = Painting.create!(name: "Painting of #{Faker::Name.first_name}", bio: "Super cool picture!", img_url: "#{Faker::LoremFlickr.image}", year: rand(2006..2023),
     user_id: rand(User.first.id..User.last.id), museum_id: rand(Museum.first.id..Museum.last.id))
 end
 
