3cf23652ffca8e345ea3c3b719763b4a3b3eeea9
Eric Song <ericksong91@gmail.com>
2023-04-04 21:03:43 -0400

    Fixed reload error on Museum page

--- client/src/components/MuseumProfile.js
+++ client/src/components/MuseumProfile.js
@@ -4,30 +4,32 @@ import Button from '@mui/material/Button';
 
 function MuseumProfile({ museums }) {
     const index = parseInt(useParams().id);
-    const museumObj = museums.find((museum)=> museum.id === index)
+    const museumObj = museums.find((museum) => museum.id === index) === undefined ?
+        []
+        :
+        museums.find((museum) => museum.id === index)
     const [paintings, setPaintings] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         museums.length === 0 ? setPaintings([]) : setPaintings(museumObj.paintings)
     }, [index, museums])
 
     //Filter paintings from Museum
-    const paintingsFilter = paintings.map((paint)=>{
+    const paintingsFilter = paintings.map((paint) => {
         return <li>{paint.name}</li>
     })
-
-    console.log(museumObj)
 
     return (
         <div className="MuseumProfile">
             <h1>You have arrived!</h1>
+
             <h2>{museumObj.location}</h2>
             <h2>{museumObj.name}</h2>
             <h3>{museumObj.bio}</h3>
 
             <h2>List of Paintings:</h2>
             {paintingsFilter}
-            
+
         </div>
     );
 }
