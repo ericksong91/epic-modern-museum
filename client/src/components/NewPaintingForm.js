363415a8a0a47848b4802c7c49259d7e27fbff3f
Eric Song <ericksong91@gmail.com>
2023-05-05 17:22:15 -0400

    Finished basic skeleton for submission

--- client/src/components/NewPaintingForm.js
+++ client/src/components/NewPaintingForm.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Grid, Container, Box, TextField, Card, CardMedia, CardHeader, CardContent } from '@mui/material';
+import {
+    Button, Grid, Container, Box, TextField, Card, CardMedia, CardHeader, CardContent,
+    FormControl, InputLabel, Select, MenuItem
+} from '@mui/material';
 
 function NewPaintingForm({ museums }) {
     const [name, setName] = useState("");
@@ -10,99 +12,116 @@ function NewPaintingForm({ museums }) {
     const [selectMuseum, setSelectMuseum] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [errors, setErrors] = useState([]);
+    const museumList = museums.map((muse) => <MenuItem key={muse.id} value={muse.name}>{muse.name}</MenuItem>);
 
     function handleSubmit(e) {
         e.preventDefault();
         setIsLoading(true);
 
-        paintObj = {
+        const paintObj = {
             name,
             bio,
             img_url: image,
             year,
-
         }
+
+        console.log(paintObj)
     }
 
     return (
         <div className="NewPaintingForm">
             <Container className='NewPaintingForm' component="main" maxWidth="xs">
-                <Grid container justifyContent={"center"}>
-                    <Grid item xs={10}>
-                        <h1>Submit New Painting</h1>
-                        <Box component="form" onSubmit={handleSubmit}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="name"
-                                label="Painting Name"
-                                name="name"
-                                autoFocus
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="bio"
-                                name="bio"
-                                label={`Description (${300 - bio.length} chars left)`}
-                                value={bio}
-                                onChange={(e) => { setBio(e.target.value) }}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="painting"
-                                name="painting"
-                                label="Painting URL (must be valid image type)"
-                                type="url"
-                                value={image}
-                                onChange={(e) => { setImage(e.target.value) }}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="year"
-                                name="year"
-                                label="Year Created (min: 1900)"
-                                type="number"
-                                inputProps={{ min: 1900, max: 2023 }}
-                                value={year}
-                                onChange={(e) => { setYear(e.target.value) }}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <h1>Submit New Painting</h1>
+                    <Box component="form" onSubmit={handleSubmit}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Painting Name"
+                            name="name"
+                            autoFocus
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="bio"
+                            name="bio"
+                            label={`Description (${300 - bio.length} chars left)`}
+                            value={bio}
+                            onChange={(e) => { setBio(e.target.value) }}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="painting"
+                            name="painting"
+                            label="Painting URL (must be valid image type)"
+                            type="url"
+                            value={image}
+                            onChange={(e) => { setImage(e.target.value) }}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="year"
+                            name="year"
+                            label="Year Created (min: 1900)"
+                            type="number"
+                            inputProps={{ min: 1900, max: 2023 }}
+                            value={year}
+                            onChange={(e) => { setYear(e.target.value) }}
+                        />
+                        <FormControl fullWidth required margin="normal">
+                            <InputLabel>Museum</InputLabel>
+                            <Select
+                                label="Museums"
+                                value={selectMuseum}
+                                id="Museums"
+                                onChange={(e) => { setSelectMuseum(e.target.value) }}
                             >
-                                {isLoading ? "Loading..." : "Submit"}
-                            </Button>
-                            {errors}
-                        </Box>
-                        <Grid item >
-                            <Card sx={{ maxWidth: 345 }}>
-                                <CardHeader
-                                    title={"Preview"}
-                                />
-                                <CardMedia
-                                    component="img"
-                                    height="400"
-                                    image={image}
-                                    alt={name}
-                                />
-                                <CardContent>
-                                    {bio}
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
-                </Grid>
+                                {museumList}
+                            </Select>
+                        </FormControl>
+                        <Card fullWidth>
+                            <CardHeader
+                                title={"Preview"}
+                                subheader={name}
+                            />
+                            <CardMedia
+                                component="img"
+                                height="400"
+                                image={image}
+                                alt={name}
+                            />
+                            <CardContent>
+                                {bio}
+                            </CardContent>
+                        </Card>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            {isLoading ? "Loading..." : "Submit"}
+                        </Button>
+                        {errors}
+                    </Box>
+                </Box>
             </Container>
         </div>
     );
--- client/src/components/top/Profile.js
+++ client/src/components/top/Profile.js
@@ -3,7 +3,6 @@ import { UserContext } from '../context/user'
 import PaintingCard from '../cards/PaintingCard';
 import MuseumCard from '../cards/MuseumCard';
 import NewPaintingForm from '../NewPaintingForm';
-import { Navigate } from 'react-router-dom';
 import { Grid, Container, Button } from '@mui/material';
 
 function Profile({ museums }) {
@@ -11,8 +10,8 @@ function Profile({ museums }) {
     const { user } = useContext(UserContext);
 
     if (!user) {
-        return <Navigate replace to="/" />;
-    };
+        return <div></div>
+    }
 
     const userPaintings = user.paintings.map((paint) => {
         return <Grid item xs={12} sm={6} md={4} key={paint.id}><PaintingCard paint={paint} /></Grid>
